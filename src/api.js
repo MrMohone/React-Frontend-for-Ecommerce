dab4aabecc5eea17f32c3d5ce7c20c0214fd2e55
MrMohone <mohammedawol0837@gmail.com>
2025-07-17 01:52:18 +0300

    user authentication commit on way

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 //to access the backend api static files like images
 export const BASE_URL = 'http://127.0.0.1:8000'
@@ -6,5 +7,23 @@ export const BASE_URL = 'http://127.0.0.1:8000'
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000' //Django backend base url
 })
+
+api.interceptors.request.use(
+    (config) =>{
+        const token = localStorage.getItem('access')
+        if(token){
+            const decoded = jwtDecode(token)
+            const expiry_date = decoded.exp *
+            const current_time = Date.now() / 1000
+            if(expiry_date > current_time){
+                config.headers.Authorization = `Bearer ${token}`
+            }
+            }
+            return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
 
 export default api
